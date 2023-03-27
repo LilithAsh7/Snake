7418e34113a87c55fcb579a3126264c6235c0275
Lilly <lilly.ashbury@gmail.com>
2023-03-27 16:20:43 -0700

    Added check for module installations and implemented installation if they are not present.

--- snake_master.py
+++ snake_master.py
@@ -1,9 +1,29 @@
-import menus
-
+import pip
+import sys
+import subprocess
 
 def main():
+    install()
+    import menus
     menus.main_menu()
 
 
+def install():
+    importlist = ["pygame", "pygame_menu", "tinydb"]
+
+    print("Checking installations...")
+
+    for lib in importlist:
+        try:
+            globals()[lib] = __import__(lib)
+            print(f"{lib} currently installed")
+        except ImportError as e:
+            print("Error -> ", e)
+            print("Installing ", lib)
+            subprocess.check_call([sys.executable, "-m", "pip", "install", lib])
+
 if __name__ == "__main__":
     main()
+
+
+
